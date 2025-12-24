843a0f88ea367dc28a68afba8b7e2453fce0ba79
AbdoX <abdoxrs223@gmail.com>
2025-12-24 03:09:03 +0200

    api feature

--- utilities/ApiFeature.js
+++ utilities/ApiFeature.js
@@ -1,39 +1,3 @@
-// exports.ApiFeature = class ApiFeature {
-//   constructor(dbQuery, requestQuery) {
-//     this.dbQuery = dbQuery;
-//     this.requestQuery = requestQuery;
-//   }
-
-//   paginate(defaultPageNumber = 1, defaultPageSize = 10) {
-//     const pageNumber = this.requestQuery.pageNumber * 1 || defaultPageNumber;
-//     const pageSize = this.requestQuery.pageSize * 1 || defaultPageSize;
-//     const skip = (pageNumber - 1) * pageSize;
-//     this.dbQuery = this.dbQuery.skip(skip).limit(pageSize);
-//     return this;
-//   }
-
-//   project() {
-//     if (this.requestQuery.project) {
-//       const projectionString =
-//         this.requestQuery.project.split(',').join(' ') + '-__v';
-//       this.dbQuery.select(projectionString);
-//     }
-//     return this;
-//   }
-
-//   sort() {
-//     if (this.requestQuery.sort) {
-//       const sortBy = this.requestQuery.sort.split(',').join(' ');
-//       this.dbQuery = this.dbQuery.sort(sortBy);
-//     }
-//     return this;
-//   }
-
-//   get dbQuery() {
-//     return this.dbQuery;
-//   }
-// };
-
 exports.ApiFeatures = class ApiFeatures {
   constructor(dbQuery, requestQeury) {
     this.dbQuery = dbQuery;
@@ -64,9 +28,3 @@ exports.ApiFeatures = class ApiFeatures {
     return this;
   }
 };
-
-// Pagination
-// pageNumber=1 & pageSize=20 -> skip 0, limit 20
-// pageNumber=2 & pageSize=20 -> skip(19), limit 20
-// pageNumber=3 & pageSize=20 -> skip(39), limit 20
-// pageNumber=4 & pageSize=20 -> skip(59), limit 20
